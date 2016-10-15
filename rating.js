499601fa870174c9492d475d9b42901c5903f6df
Gina <gdebell@gmail.com>
2016-10-15 15:11:52 -0600

    Thumb up icon will increase the score by one when you click on the icon.  Thumb down icon will decrease the score by one when you click on the icon.

---
+++ rating.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  var app = angular.module('redditCreatorApp')
+  .controller('CreateRatingController', CreateRatingController)
+
+  function CreateRatingController ($rootScope) {
+
+    this.increaseRating = (shop) => {
+      shop.rating ++;
+
+
+    }
+
+    this.decreaseRating = (shop) => {
+      shop.rating --;
+    }
+
+
+  }
+
+})()
