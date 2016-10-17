19acf8505930990dc39d05733bb2e36e8e9d3195
Gina <gdebell@gmail.com>
2016-10-17 09:01:43 -0600

    The comments display for each ice cream shop.  The comments are no longer hard coded in but are added dynamically

--- rating.js
+++ rating.js
@@ -23,14 +23,5 @@
 })()
 
 
-
-//   <span ng-class="{'color-red': s.state === 'Error', 'color-blue': s.state === 'Warning', 'color-green': s.state === 'Ignored'}">{{s.state}}</span>
-// </li>
-
-
-//
-// <div ng-class='{highlight: 4 + 4 === 8}'> 4 + 4 = 8</div>
-
 // Icons should be considered view-only elements that should not be used directly as buttons; instead nest a <md-icon> inside a md-button to add hover and click features.
 //
-// <md-icon md-svg-src="icon.svg" style=" color: rgb(244,67,54)"></md-icon>
