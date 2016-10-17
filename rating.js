9e874e5906d937217c165aec2e58bd6bbd75399a
Gina <gdebell@gmail.com>
2016-10-16 22:35:26 -0600

    The background color for postive number is green.  Any number = to 0 or less than zero the background color is red.

--- rating.js
+++ rating.js
@@ -8,6 +8,8 @@
 
     this.increaseRating = (shop) => {
       shop.rating ++;
+      console.log(shop.rating);
+      console.log(typeof shop.rating);
     }
 
     this.decreaseRating = (shop) => {
@@ -20,9 +22,15 @@
 
 })()
 
+
+
+//   <span ng-class="{'color-red': s.state === 'Error', 'color-blue': s.state === 'Warning', 'color-green': s.state === 'Ignored'}">{{s.state}}</span>
+// </li>
+
+
 //
 // <div ng-class='{highlight: 4 + 4 === 8}'> 4 + 4 = 8</div>
 
 // Icons should be considered view-only elements that should not be used directly as buttons; instead nest a <md-icon> inside a md-button to add hover and click features.
-// 
+//
 // <md-icon md-svg-src="icon.svg" style=" color: rgb(244,67,54)"></md-icon>
